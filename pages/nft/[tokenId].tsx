3700569d741c804ec79dd3f756908f5de0751846
kksam <1011847054@qq.com>
2024-12-07 00:30:38 +0800

    fix:bug

--- pages/nft/[tokenId].tsx
+++ pages/nft/[tokenId].tsx
@@ -1,22 +1,19 @@
-import { useRouter } from 'next/router';
-import Navbar from '../../components/Navbar';
-import { NFTDisplay } from '../../components/NFTDisplay';
+import { useRouter } from "next/router";
+import Navbar from "../../components/Navbar";
+import { NFTDisplay } from "../../components/NFTDisplay";
 
 export default function NFTPage() {
-    const router = useRouter();
-    const { tokenId } = router.query;
+  const router = useRouter();
+  const { tokenId } = router.query;
 
-    return (
-        <div className="min-h-screen bg-gray-100 flex flex-col">
-            <Navbar />
-            <main className="flex-1 container mx-auto px-4 py-8">
-                {tokenId && (
-                    <NFTDisplay
-                        tokenId={tokenId as string}
-                        className="mb-8"
-                    />
-                )}
-            </main>
-        </div>
-    );
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      <Navbar />
+      <main className="flex-1 container mx-auto px-4 py-8">
+        {/* {tokenId && (
+          <NFTDisplay nft={} tokenId={tokenId as string} className="mb-8" />
+        )} */}
+      </main>
+    </div>
+  );
 }
