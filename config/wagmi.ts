8e05538d715be949f466e58203e2bffcf140ec7f
kksam <1011847054@qq.com>
2024-12-07 01:08:26 +0800

    fix:update abi

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -1,23 +1,23 @@
-import { getDefaultWallets } from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig } from 'wagmi';
-import { sepolia } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
+import { getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig } from "wagmi";
+import { mainnet } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
 
 export const { chains, publicClient } = configureChains(
-    [sepolia],
-    [publicProvider()]
+  [mainnet],
+  [publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-    appName: 'Neural Drift',
-    projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || '',
-    chains,
+  appName: "Neural Drift",
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "",
+  chains,
 });
 
 const wagmiConfig = createConfig({
-    autoConnect: true,
-    connectors,
-    publicClient,
+  autoConnect: true,
+  connectors,
+  publicClient,
 });
 
 export default wagmiConfig;
